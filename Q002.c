a009b278f0d8710427ed4956a7cc9072ad670a4f
Sakshi Girotti <sakshigirotti@gmail.com>
2026-08-15 19:59:31 +0530

    Add files via upload

---
+++ Q002.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() { int a,b;
+printf("enter 2 values");
+scanf("%d,%d",&a,&b);
+printf("sum = %d",a+b);
+printf("diff=%d",a-b);
+printf("product=%d",a*b);
+printf("quotient=%d",a/b);
+
+    return 0;
+
+}
