8a8a8b6faee5bfe1a7c142c9c9cb3be9a27de484
Sakshi Girotti <sakshigirotti@gmail.com>
2026-08-22 22:48:16 +0530

    Add files via upload

---
+++ q6.c
@@ -0,0 +1,16 @@
+//Q6: Write a program to swap two numbers using a third variable.//
+#include<stdio.h>
+int main()
+{
+    int a, b, temp;
+    printf("Enter two numbers: ");
+    scanf("%d %d", &a, &b);
+    
+    // Swapping using a third variable
+    temp = a;
+    a = b;
+    b = temp;
+    
+    printf("After swapping: a = %d, b = %d", a, b);
+    return 0;
+}
