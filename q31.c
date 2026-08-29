4705bec71bf5e4bb042a30670be49cb41348bf01
Sakshi Girotti <sakshigirotti@gmail.com>
2026-08-29 18:52:59 +0530

    Add files via upload

---
+++ q31.c
@@ -0,0 +1,27 @@
+//Q31: Write a program to take a number as input and print its equivalent binary representation.//
+#include<stdio.h>
+int main()
+{
+    int num, binary[32], i = 0;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    if(num == 0) {
+        printf("Binary representation: 0\n");
+        return 0;
+    }
+
+    while(num > 0) {
+        binary[i] = num % 2;
+        num = num / 2;
+        i++;
+    }
+
+    printf("Binary representation: ");
+    for(int j = i - 1; j >= 0; j--) {
+        printf("%d", binary[j]);
+    }
+    printf("\n");
+
+    return 0;
+}
