3c65706f9c7ffcace8413c30410b502425e311eb
Sakshi Girotti <sakshigirotti@gmail.com>
2026-09-13 12:32:15 +0530

    Add files via upload

---
+++ q52.c
@@ -0,0 +1,51 @@
+/*Q52: Write a program to print the following pattern:
+
+*
+
+*
+*
+*
+
+*
+*
+*
+*
+*
+
+*
+*
+*
+
+*   */
+
+#include<stdio.h>
+int main()
+{
+    int i,j;
+    for(i=1;i<=4;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=5;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    for(i=1;i<=3;i++)
+    {
+        for(j=1;j<=i;j++)
+        {
+            printf("*\n");
+        }
+        printf("\n");
+    }
+    printf("*\n");
+    return 0;
+}   
